04f9bf307b88f3a82699f8049226f22ea35470a1
Daniil <98778382+DanVilcka@users.noreply.github.com>
2024-01-09 19:29:02 +0300

    Initial Commit

---
+++ RGBApp/RGBAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  RGBAppApp.swift
+//  RGBApp
+//
+//  Created by Daniil Vilchinskiy on 09.01.2024.
+//
+
+import SwiftUI
+
+@main
+struct RGBAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
